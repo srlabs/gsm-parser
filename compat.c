5d19e5aed30ef21d97c16c539ac1042e74327df5
Jonas Schmid <jonas@srlabs.de>
2019-06-21 16:31:26 +0200

    [PATCH] Fix compat.c in gsm-parser for 64-bit builds

--- compat.c
+++ compat.c
@@ -2,21 +2,28 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <time64.h>
 
-#define CHAR_BIT 8
-// From http://code.metager.de/source/xref/chromium/base/os_compat_android.cc
-// 32-bit Android has only timegm64() and not timegm().
-// We replicate the behaviour of timegm() when the result overflows time_t.
-time_t timegm(struct tm* const t) {
-  // time_t is signed on Android.
-  static const time_t kTimeMax = ~(1L << (sizeof(time_t) * CHAR_BIT - 1));
-  static const time_t kTimeMin = (1L << (sizeof(time_t) * CHAR_BIT - 1));
-  time64_t result = timegm64(t);
-  if (result < kTimeMin || result > kTimeMax)
-    return -1;
-  return result;
-}
+#if defined(__LP64__)
+  // We are on 64-bit already => No need to emulate anything special
+  #define timegm(x) timegm64(x)
+#else
+  // Loading time64.h fails with #error on 64-bit => Only load it on 32-bit
+  #include <time64.h>
+
+  #define CHAR_BIT 8
+  // From http://code.metager.de/source/xref/chromium/base/os_compat_android.cc
+  // 32-bit Android has only timegm64() and not timegm().
+  // We replicate the behaviour of timegm() when the result overflows time_t.
+  time_t timegm(struct tm* const t) {
+    // time_t is signed on Android.
+    static const time_t kTimeMax = ~(1L << (sizeof(time_t) * CHAR_BIT - 1));
+    static const time_t kTimeMin = (1L << (sizeof(time_t) * CHAR_BIT - 1));
+    time64_t result = timegm64(t);
+    if (result < kTimeMin || result > kTimeMax)
+      return -1;
+    return result;
+  }
+#endif
 
 // https://android.googlesource.com/platform/external/elfutils/+/android-4.3.1_r1/bionic-fixup/getline.c
 ssize_t getline(char **lineptr, size_t *n, FILE *stream)
